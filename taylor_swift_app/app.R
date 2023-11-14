7895c5adb3574926ae2564efa216d3065a0fe5a3
Anu Jajodia <53088267+AJajodia@users.noreply.github.com>
2023-11-14 13:45:11 -0600

    added anu_ts_ideas

--- taylor_swift_app/app.R
+++ taylor_swift_app/app.R
@@ -11,6 +11,43 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 
+### data sets
+taylor_album_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_album_songs.csv')
+taylor_all_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_all_songs.csv')
+taylor_albums <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_albums.csv')
+
+taylor_album_focus <- taylor_all_songs %>%
+  # full_join(taylor_albums, by = c("album_name", "ep", "album_release")) %>%
+  filter(!(is.na(album_name))) %>%
+  filter(!(album_name %in% c("The Taylor Swift Holiday Collection", "Beautiful Eyes"))) %>%
+  mutate(album_name = as.character(album_name)) %>%
+  group_by(album_name) %>%
+  mutate(album_duration = sum(duration_ms, na.rm = T)/60000,
+         album_danceability = sum(danceability, na.rm = T)/max(track_number),
+         album_energy = sum(energy, na.rm = T)/max(track_number),
+         album_loudness = sum(loudness, na.rm = T)/max(track_number),
+         album_speechiness = sum(speechiness, na.rm = T)/max(track_number),
+         album_acousticness = sum(acousticness, na.rm = T)/max(track_number),
+         album_instrumentalness = sum(instrumentalness, na.rm = T)/max(track_number),
+         album_liveness = sum(liveness, na.rm = T)/max(track_number),
+         album_valence = sum(valence, na.rm = T)/max(track_number),
+         album_tempo = sum(tempo, na.rm = T)/max(track_number),
+         album_track_number = max(track_number),
+         # album_metacritic_score = metacritic_score,
+         # album_user_score = user_score
+  ) %>%
+  ungroup()
+
+all_album_palette <- c("lightseagreen", "darkgoldenrod1", "mediumorchid", 
+                       "red", "cornflowerblue", "black", "hotpink",  "ivory4", 
+                       "darkorange3", "darkgoldenrod3" ,"red3","navyblue")
+
+all_songs_palette <- c("lightseagreen", "forestgreen", "darkorange","darkgoldenrod1", "mediumorchid", 
+                       "red", "cornflowerblue", "black", "hotpink",  "ivory4", 
+                       "darkorange3", "darkgoldenrod3" ,"red3","navyblue", "gray99")
+###
+
+
 album_vars <- taylor_album_focus %>% select(starts_with("album_"), -c(album_name))
 
 # Define UI for application that draws a histogram
@@ -57,41 +94,6 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-### data sets
-  taylor_album_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_album_songs.csv')
-  taylor_all_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_all_songs.csv')
-  taylor_albums <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-17/taylor_albums.csv')
-  
-  taylor_album_focus <- taylor_all_songs %>%
-    # full_join(taylor_albums, by = c("album_name", "ep", "album_release")) %>%
-    filter(!(is.na(album_name))) %>%
-    filter(!(album_name %in% c("The Taylor Swift Holiday Collection", "Beautiful Eyes"))) %>%
-    mutate(album_name = as.character(album_name)) %>%
-    group_by(album_name) %>%
-    mutate(album_length_min = sum(duration_ms, na.rm = T)/60000,
-           album_danceability = sum(danceability, na.rm = T)/max(track_number),
-           album_energy = sum(energy, na.rm = T)/max(track_number),
-           album_loudness = sum(loudness, na.rm = T)/max(track_number),
-           album_speechiness = sum(speechiness, na.rm = T)/max(track_number),
-           album_acousticness = sum(acousticness, na.rm = T)/max(track_number),
-           album_instrumentalness = sum(instrumentalness, na.rm = T)/max(track_number),
-           album_liveness = sum(liveness, na.rm = T)/max(track_number),
-           album_valence = sum(valence, na.rm = T)/max(track_number),
-           album_tempo = sum(tempo, na.rm = T)/max(track_number),
-           album_track_number = max(track_number),
-           # album_metacritic_score = metacritic_score,
-           # album_user_score = user_score
-    ) %>%
-    ungroup()
-  
-  all_album_palette <- c("lightseagreen", "darkgoldenrod1", "mediumorchid", 
-                         "red", "cornflowerblue", "black", "hotpink",  "ivory4", 
-                         "darkorange3", "darkgoldenrod3" ,"red3","navyblue")
-  
-  all_songs_palette <- c("lightseagreen", "forestgreen", "darkorange","darkgoldenrod1", "mediumorchid", 
-                         "red", "cornflowerblue", "black", "hotpink",  "ivory4", 
-                         "darkorange3", "darkgoldenrod3" ,"red3","navyblue", "gray99")
-###
 
     output$albumPlot <- renderPlot({
       taylor_album_focus %>%
